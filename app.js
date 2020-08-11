b600a1eaeb3900e791580572a9dd98a477c21c91
ansh75453 <69009256+ansh75453@users.noreply.github.com>
2020-08-11 16:27:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyD9aPLEDkFoS_niiNK46GwTGA6IPnri2EA",
+  authDomain: "ansh-4d1e8.firebaseapp.com",
+  databaseURL: "https://ansh-4d1e8.firebaseio.com",
+  projectId: "ansh-4d1e8",
+  storageBucket: "ansh-4d1e8.appspot.com",
+  messagingSenderId: "361143192128",
+  appId: "1:361143192128:web:f753a6d667e8fc6afd6da5",
+  measurementId: "G-5XHT5MD7X1"
 };
 
      
