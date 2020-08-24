4ef810a64e3b3dabd47b1f411beabfab610a9072
ansh75453 <69009256+ansh75453@users.noreply.github.com>
2020-08-24 11:03:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyAACnSzeJmKuGnvYiDe1379dMJfsFL8Vyc",
+  authDomain: "instagram-ba982.firebaseapp.com",
+  databaseURL: "https://instagram-ba982.firebaseio.com",
+  projectId: "instagram-ba982",
+  storageBucket: "instagram-ba982.appspot.com",
+  messagingSenderId: "971596039899",
+  appId: "1:971596039899:web:e00cdb7a159bc6ea81eefe",
+  measurementId: "G-K2L2R5PX63"
 };
 
      
